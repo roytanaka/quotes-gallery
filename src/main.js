8ebb9073fd7e11a77cb1f0dbac0413488da6cb65
Roy Tanaka <tanaka@mac.com>
2021-04-27 16:02:12 -0400

    build commands

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import Axios from 'axios';
 
-Axios.defaults.baseURL = process.env.VUE_APP_QUOTES_URL;
+Axios.defaults.baseURL = '.netlify/functions';
 Axios.defaults.headers.get['Accept'] = 'application/json';
 
 Vue.config.productionTip = false;
