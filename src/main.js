5d63000cd3a4063914a61f57e342d6951c96437f
Roy Tanaka <tanaka@mac.com>
2021-04-27 12:41:22 -0400

    axios config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
+import Axios from 'axios';
+
+Axios.defaults.baseURL = process.env.VUE_APP_QUOTES_URL;
+Axios.defaults.headers.get['Accept'] = 'application/json';
 
 Vue.config.productionTip = false;
 
