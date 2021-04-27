8adab43c3a5e789206a157383ad6c642d8f5e9a1
Roy Tanaka <tanaka@mac.com>
2021-04-27 12:41:32 -0400

    fetch quotes

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,28 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ul>
+      <li v-for="quote in quotes" :key="quote.text">
+        {{ quote.text }} - {{ quote.author }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
-
+import Axios from 'axios';
 export default {
   name: 'App',
   components: {
     HelloWorld,
+  },
+  data: () => ({
+    quotes: [],
+  }),
+  async created() {
+    const response = await Axios.get('getQuotes');
+    this.quotes = response.data.quotes;
   },
 };
 </script>
