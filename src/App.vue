caa5810b9c9f2897ad6b7bf140c1517dc4ad89be
Roy Tanaka <tanaka@mac.com>
2021-04-28 23:23:42 -0400

    spinner

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,28 @@
     <transition name="spinner">
       <SpinnerLoader v-if="loading" />
     </transition>
+    <transition appear v-on:appear="customAppearHook" v-if="!loading">
+      <img class="swipe-icon" src="@/assets/Tap.svg" alt="" aria-hidden />
+    </transition>
     <transition @enter="enter" @leave="leave" :css="false" mode="out-in">
       <QuoteBox
         :key="currentQuote"
         v-if="quotes.length"
         :text="quote.text"
         :author="quote.author"
-        @prev-handler="currentQuote = currentQuote - 1"
-        @next-handler="currentQuote = currentQuote + 1"
+        @prev-handler="previous"
+        @next-handler="next"
       >
         <template v-slot:controls="{ on }">
           <SlideBtn
             type="prev"
             aria-label="previous quote"
             @click="on.prevHandler"
-            :disabled="isFirstQuote"
           />
           <SlideBtn
             type="next"
             aria-label="next quote"
             @click="on.nextHandler"
-            :disabled="isLastQuote"
           />
         </template>
       </QuoteBox>
@@ -47,7 +48,7 @@ export default {
   }),
   async created() {
     this.loading = true;
-    const response = await Axios.post('getQuotes');
+    const response = await Axios.post('getQuotes', { count: 100 });
     this.loading = false;
     this.quotes = response.data.quotes;
   },
@@ -60,6 +61,31 @@ export default {
     });
   },
   methods: {
+    previous() {
+      if (this.isFirstQuote) {
+        this.currentQuote = this.quotes.length - 1;
+      } else {
+        this.currentQuote = this.currentQuote - 1;
+      }
+    },
+    next() {
+      if (this.isLastQuote) {
+        this.currentQuote = 0;
+      } else {
+        this.currentQuote = this.currentQuote + 1;
+      }
+    },
+    customAppearHook(el, done) {
+      const tl = gsap.timeline({
+        onComplete: done,
+        defaults: { duration: 1.2, ease: 'sine.inOut', delay: 0.4 },
+      });
+      tl.to(el, { delay: 3.5, opacity: 1 })
+        .to(el, { rotate: -30, delay: 0 }, '-=0.8')
+        .to(el, { rotate: 30 })
+        .to(el, { rotate: -30 })
+        .to(el, { opacity: 0 }, '-=2.8');
+    },
     enter(el, done) {
       const blockquote = el.querySelector('blockquote');
       const frames = el.querySelectorAll('.frame');
@@ -72,12 +98,24 @@ export default {
       });
 
       tl.from(frames, { width: 0 }, '+=0')
-        .from(quoteChar[0], { opacity: 0, rotate: -135 }, '-=0.1')
-        .from(quoteChar[1], { opacity: 0, rotate: 135 }, '<')
+        .from(
+          quoteChar,
+          {
+            opacity: 0,
+            rotateY: '+=720',
+            ease: 'expo.out',
+            duration: 2.5,
+            stagger: {
+              each: 0.15,
+              from: 'random',
+            },
+          },
+          '-=0.1'
+        )
         .from(
           blockquote,
           { opacity: 0, yPercent: -20, ease: 'power1.out' },
-          '-=0.3'
+          '-=2.2'
         )
         .from(
           figcaption,
@@ -86,10 +124,7 @@ export default {
         );
     },
     leave(el, done) {
-      gsap.to(el, {
-        opacity: 0,
-        onComplete: done,
-      });
+      gsap.to(el, { opacity: 0, onComplete: done });
     },
   },
   computed: {
@@ -107,17 +142,22 @@ export default {
     quote() {
       document.title = `A quote by ${this.quote.author || 'Anonymous'}`;
     },
-    async currentQuote(val) {
-      if (val === this.quotes.length - 5) {
-        const response = await Axios.post('getQuotes', { count: 5 });
-        this.quotes.push(...response.data.quotes);
-      }
-    },
   },
 };
 </script>
 
 <style lang="scss">
+.full-screen {
+  position: relative;
+}
+.swipe-icon {
+  position: absolute;
+  width: clamp(2.5em, 7vw, 5em);
+  transform-origin: 50% 200%;
+  transform: rotate(30deg);
+  opacity: 0;
+}
+
 .spinner-enter-active,
 .spinner-leave-active {
   transition: opacity 0.3s;
--- src/components/SpinnerLoader.vue
+++ src/components/SpinnerLoader.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="backdrop">
-    <div class="spinner-container">
-      <span class="spinner">“</span>
-      <span class="spinner">”</span>
-    </div>
+    <div class="spinner-container">“”</div>
   </div>
 </template>
 
@@ -12,22 +9,13 @@ import gsap from 'gsap';
 
 export default {
   mounted() {
-    gsap.to('.spinner', {
-      duration: 2,
-      rotateY: '+=1080',
+    gsap.to('.spinner-container', {
+      duration: 1.5,
+      rotate: '+=1080',
       ease: 'expo.out',
       repeat: -1,
       transformOrigin: '50% 35%',
-      stagger: {
-        each: 0.1,
-        from: 'random',
-      },
     });
   },
 };
 </script>
-<style scoped>
-.spinner {
-  display: inline-block;
-}
-</style>
